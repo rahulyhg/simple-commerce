eb93c0048535a6af42344afa39eb6bf6dc0924c0
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-07-10 19:42:07 +0300

    Fix TreasuryPaper row

--- admin/src/Components/Pages/TreasuryPapers/TreasuryPaperRow.js
+++ admin/src/Components/Pages/TreasuryPapers/TreasuryPaperRow.js
@@ -4,18 +4,25 @@ import Label from '../../Label';
 class TreasuryPaperRow extends Component {
     render() {
         const model = this.props.model;
+
+        let content;
+
+        if (model.accountable.data.model_type === 'product' ){
+            content = `Product: ${model.accountable.data.title}`;
+        }else{
+            content = `Order #${model.accountable.data.id}`;
+        }
         return (
             <tr>
                 <td>{this.props.index+1}</td>
                 <td>
-                    {model.type === 'Modules\ShoppingCart\Entities\Order' && `Order #${model.accountable.data.id}`}
-                    {model.type === 'Modules\ShoppingCart\Entities\Product' && `Product: ${model.accountable.data.title}`}
+                    {content}
                 </td>
                 <td>
                     {model.type === 'in' && <Label type="success">In</Label>}
                     {model.type === 'out' && <Label type="danger">Out</Label>}
                 </td>
-                <td>{model.type}</td>
+                <td>{model.value}</td>
                 <td>{model.created_at}</td>
             </tr>
         );
